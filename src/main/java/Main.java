2d0a8fad81047e04f8819d59bcbedf925aa1db74
Ajyamkar <ajyamkar99@gmail.com>
2025-09-20 14:49:03 +0530

    Respond to multiple ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,41 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-
-          OutputStream outputStream = clientSocket.getOutputStream();
-          outputStream.write("+PONG\r\n".getBytes());
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             // Wait for connection from client.
+             Socket clientSocket = serverSocket.accept();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             OutputStream outputStream = clientSocket.getOutputStream();
+        ) {
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            String content;
+//            int i=0;
+//            int messageLength = 1;
+            while ((content = reader.readLine())!=null){
+                System.out.println("content: "+content);
+//                if(content.substring(content.length()-2).contains("*")){
+//                    messageLength=Integer.parseInt(content.substring(content.length()-1)) * 2;
+//                }
+                if("PING".equals(content)) {
+                    outputStream.write("+PONG\r\n".getBytes());
+                    outputStream.flush();
+//                    i=0;
+                }
+//                i++;
             }
-          } catch (IOException e) {
+
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
         }
-  }
+    }
 }
