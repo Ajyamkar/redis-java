678302d3e0dc43622dcf2e7e5fee2e48438ed046
Ajyamkar <ajyamkar99@gmail.com>
2025-09-20 16:00:13 +0530

    Handled concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import com.redis.java.RedisServer;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,28 +14,26 @@ public class Main {
 
         int port = 6379;
         try (ServerSocket serverSocket = new ServerSocket(port);
-             // Wait for connection from client.
-             Socket clientSocket = serverSocket.accept();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             OutputStream outputStream = clientSocket.getOutputStream();
+             ExecutorService executor = Executors.newFixedThreadPool(4)
         ) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            String content;
-//            int i=0;
-//            int messageLength = 1;
-            while ((content = reader.readLine())!=null){
-                System.out.println("content: "+content);
-//                if(content.substring(content.length()-2).contains("*")){
-//                    messageLength=Integer.parseInt(content.substring(content.length()-1)) * 2;
-//                }
-                if("PING".equals(content)) {
-                    outputStream.write("+PONG\r\n".getBytes());
-                    outputStream.flush();
-//                    i=0;
-                }
-//                i++;
+
+            // Continuous server running...
+            while (true){
+                // Wait for connection from client.
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New connection");
+                executor.submit(()-> {
+
+                    RedisServer redisServer = new RedisServer(clientSocket);
+                    try {
+                        redisServer.execute();
+                    } catch (IOException e) {
+                        System.out.println("IOException calling redis server: " + e.getMessage());
+                    }
+                });
             }
 
         } catch (IOException e) {
